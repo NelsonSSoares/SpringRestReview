ddf4eb86064c4f7eae029364bfda77f926d2b980
Nelson <punkpunkada@gmail.com>
2025-02-19 08:43:13 -0300

    returning a json list

--- src/main/java/br/com/nelsonssoares/springreview/controllers/PersonController.java
+++ src/main/java/br/com/nelsonssoares/springreview/controllers/PersonController.java
@@ -3,10 +3,9 @@ package br.com.nelsonssoares.springreview.controllers;
 import br.com.nelsonssoares.springreview.models.Person;
 import br.com.nelsonssoares.springreview.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/person", produces = "application/json")
@@ -19,6 +18,11 @@ public class PersonController {
     @PostMapping("/{id}")
     public Person findById(@PathVariable("id") String id) {
         return personService.findById(id);
+    }
+
+    @GetMapping
+    public List<Person> findAll(){
+        return personService.findAll();
     }
 
 }
--- src/main/java/br/com/nelsonssoares/springreview/services/PersonService.java
+++ src/main/java/br/com/nelsonssoares/springreview/services/PersonService.java
@@ -5,6 +5,8 @@ import br.com.nelsonssoares.springreview.models.Person;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -25,6 +27,27 @@ public class PersonService {
         person.setAddress("Rua 1");
         person.setGender("Male");
 
+        return person;
+    }
+
+    public List<Person> findAll(){
+        List<Person> persons = new ArrayList<Person>();
+
+        for(int i =0 ; i < 8; i++){
+
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name" + i);
+        person.setLastName("Last Name" + i);
+        person.setAddress("Some Address in Brasil" + i);
+        person.setGender("Male");
         return person;
     }
 }
