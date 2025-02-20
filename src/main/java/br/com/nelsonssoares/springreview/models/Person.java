1aa871218cb52459465afcd4d72330169ca73e4d
Nelson <punkpunkada@gmail.com>
2025-02-20 14:26:54 -0300

    add jpa anotations and repository

--- src/main/java/br/com/nelsonssoares/springreview/models/Person.java
+++ src/main/java/br/com/nelsonssoares/springreview/models/Person.java
@@ -1,5 +1,6 @@
 package br.com.nelsonssoares.springreview.models;
 
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,18 +10,26 @@ import java.io.Serializable;
 import java.util.Objects;
 
 
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@EqualsAndHashCode
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 
 
---
+++ src/main/java/br/com/nelsonssoares/springreview/repositories/ParsonRepository.java
@@ -0,0 +1,10 @@
+package br.com.nelsonssoares.springreview.repositories;
+
+import br.com.nelsonssoares.springreview.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParsonRepository extends JpaRepository<Person, Long> {
+
+}
