f47534f42ec87fbd044f27974e9d10a9c79eeb97
Nelson <punkpunkada@gmail.com>
2025-04-02 18:34:19 -0300

    crating CORS Tests

---
+++ src/test/java/br/com/nelsonssoares/springreview/controllers/withjson/PersonControllerTest.java
@@ -0,0 +1,102 @@
+package br.com.nelsonssoares.springreview.controllers.withjson;
+
+import br.com.nelsonssoares.springreview.config.tests.TestConfigs;
+import br.com.nelsonssoares.springreview.integrationtests.AbstractIntegrationTest;
+import br.com.nelsonssoares.springreview.integrationtests.dto.PersonDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class PersonControllerTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+    private static PersonDTO personDTO;
+
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        personDTO = new PersonDTO();
+    }
+    private void mockPerson(){
+        personDTO.setFirstName("Richard");
+        personDTO.setLastName("Nixon");
+        personDTO.setAddress("White House");
+        personDTO.setGender("Male");
+    }
+
+    @Order(1)
+    @Test
+    void create() throws JsonProcessingException {
+        mockPerson();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.HEADER_PARAM_ORIGIN_LOCAL)
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        var content = given(specification)
+
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(personDTO)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        PersonDTO created = objectMapper.readValue(content, PersonDTO.class);
+        personDTO = created;
+
+        assertNotNull(created.getId());
+        assertNotNull(created.getFirstName());
+        assertNotNull(created.getLastName());
+        assertNotNull(created.getAddress());
+        assertNotNull(created.getGender());
+
+        assertTrue(created.getId() > 0);
+
+        assertEquals("Richard", created.getFirstName());
+        assertEquals("Nixon", created.getLastName());
+        assertEquals("White House" , created.getAddress());
+        assertEquals("Male", created.getGender());
+    }
+
+    @Test
+    void findById() {
+    }
+
+
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
---
+++ src/test/java/br/com/nelsonssoares/springreview/integrationtests/dto/PersonDTO.java
@@ -0,0 +1,78 @@
+package br.com.nelsonssoares.springreview.integrationtests.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonDTO implements Serializable  {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String gender;
+
+
+
+    public PersonDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTO personDTO = (PersonDTO) o;
+        return Objects.equals(id, personDTO.id) && Objects.equals(firstName, personDTO.firstName) && Objects.equals(lastName, personDTO.lastName)  && Objects.equals(address, personDTO.address) && Objects.equals(gender, personDTO.gender) ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
+    }
+}
