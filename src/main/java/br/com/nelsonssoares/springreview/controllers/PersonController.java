3932e7936a0fe31abebeccecff1e58fbc6096833
Nelson <punkpunkada@gmail.com>
2025-04-18 09:41:36 -0300

    adding HAL and HATEOAS at pagination

--- src/main/java/br/com/nelsonssoares/springreview/controllers/PersonController.java
+++ src/main/java/br/com/nelsonssoares/springreview/controllers/PersonController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,7 +44,7 @@ public class PersonController implements PersonControllerInterface {
 
     @Override
     @GetMapping
-    public ResponseEntity<Page<PersonDTO>> findAll(
+    public ResponseEntity<PagedModel<EntityModel<PersonDTO>>> findAll(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "size", defaultValue = "12") Integer size,
             @RequestParam(value = "direction", defaultValue = "asc") String direction
