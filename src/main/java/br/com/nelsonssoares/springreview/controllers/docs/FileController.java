6dba07f3830951e34ba85d88188693776b4720e0
Nelson <punkpunkada@gmail.com>
2025-04-25 15:02:08 -0300

    upload endpoint completed

--- src/main/java/br/com/nelsonssoares/springreview/controllers/docs/FileController.java
+++ src/main/java/br/com/nelsonssoares/springreview/controllers/docs/FileController.java
@@ -6,14 +6,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import br.com.nelsonssoares.springreview.services.FileStorageService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/file")
+@RequestMapping("/api/file/v1")
 public class FileController implements FileControllerDocs{
 
     private static final Logger logger = LoggerFactory.getLogger(FileController.class);
@@ -21,9 +26,20 @@ public class FileController implements FileControllerDocs{
     @Autowired
     public FileStorageService fileService;
 
+    @PostMapping("/uploadFile")
     @Override
-    public UploadFileResponseDTO uploadFile(MultipartFile file) {
-        return null;
+    public UploadFileResponseDTO uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+
+        var fileName = fileService.storeFile(file);
+        // cria a URI para download do arquivo
+        // o caminho do arquivo é o mesmo do upload, mas com o nome do arquivo
+        // fromCurrentContextPath() retorna o caminho atual do contexto da aplicação por exemplo http://localhost:8080
+        var fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new UploadFileResponseDTO(fileName, fileDownloadUri, file.getContentType(), file.getSize());
     }
 
     @Override
