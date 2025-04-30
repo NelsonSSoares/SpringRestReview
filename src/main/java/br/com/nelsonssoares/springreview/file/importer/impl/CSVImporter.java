89f698b7cf53155f7aba40e2eca774eab3d98e0d
Nelson <punkpunkada@gmail.com>
2025-04-30 17:34:26 -0300

    creating factory to choose correctly importer csv/xlsx

---
+++ src/main/java/br/com/nelsonssoares/springreview/file/importer/impl/CSVImporter.java
@@ -0,0 +1,14 @@
+package br.com.nelsonssoares.springreview.file.importer.impl;
+
+import br.com.nelsonssoares.springreview.domain.dtos.v1.PersonDTO;
+import br.com.nelsonssoares.springreview.file.importer.contract.FileImporter;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class CSVImporter implements FileImporter {
+    @Override
+    public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
+        return List.of();
+    }
+}
