5cc5bda479abcaf0f36ddf32c91a1050319a513e
Nelson <punkpunkada@gmail.com>
2025-05-14 14:51:25 -0300

    creating config class to send emails

---
+++ src/main/java/br/com/nelsonssoares/springreview/config/email/EmailConfig.java
@@ -0,0 +1,22 @@
+package br.com.nelsonssoares.springreview.config.email;
+
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.email")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class EmailConfig {
+
+    private String host;
+    private String port;
+    private String username;
+    private String password;
+    private String from;
+    private boolean ssl;
+
+}
