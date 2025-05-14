8abb7ce925bae53e241fcc542d3d9a79ce16d330
Nelson <punkpunkada@gmail.com>
2025-05-14 15:42:28 -0300

    EmailSender completed

--- src/main/java/br/com/nelsonssoares/springreview/mail/EmailSender.java
+++ src/main/java/br/com/nelsonssoares/springreview/mail/EmailSender.java
@@ -1,8 +1,13 @@
 package br.com.nelsonssoares.springreview.mail;
 
+import br.com.nelsonssoares.springreview.config.email.EmailConfig;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -10,8 +15,12 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @Component
 public class EmailSender implements Serializable {
+
+    Logger logger = getLogger(EmailSender.class);
 
     private final JavaMailSender mailSender;
     private String to;
@@ -30,24 +39,72 @@ public class EmailSender implements Serializable {
         return this;
     }
 
-    public void setSubject(String subject) {
+    public EmailSender withSubject(String subject) {
         this.subject = subject;
+        return this;
     }
 
-    public void setBody(String body) {
+    public EmailSender withMessage(String body) {
         this.body = body;
+        return this;
     }
 
     public void setFrom(String from) {
         this.from = from;
     }
 
-    public void setRecipients(ArrayList<InternetAddress> recipients) {
-        this.recipients = recipients;
+
+
+    public EmailSender setAttachment(String fileDir) {
+        this.attachment = new File(fileDir);
+        return this;
     }
 
-    public void setAttachment(File attachment) {
-        this.attachment = attachment;
+    public void send(EmailConfig config) {
+        // Implementar o envio de email aqui
+        // Exemplo: mailSender.send(mimeMessage);
+
+        logger.info("Enviando email para: " + recipients);
+        logger.info("Assunto: " + subject);
+        logger.info("Mensagem: " + body);
+        logger.info("De: " + config.getFrom());
+        logger.info("Para: " + recipients);
+        logger.info("Anexo: " + attachment);
+
+        if (attachment != null) {
+            System.out.println("Anexo: " + attachment.getAbsolutePath());
+        }
+
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+
+        try {
+            helper = new MimeMessageHelper(message, true);
+            helper.setFrom(config.getUsername());
+            helper.setTo(recipients.toArray(new InternetAddress[0]));
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            if (attachment != null) {
+                helper.addAttachment(attachment.getName(), attachment);
+            }
+
+            mailSender.send(message);
+
+            reset();
+
+        } catch (MessagingException e) {
+            throw new RuntimeException("error sending the email",e);
+        }
+
+    }
+    private void reset() {
+        this.to = null;
+        this.subject = null;
+        this.body = null;
+        this.from = null;
+        this.recipients = new ArrayList<>();
+        this.attachment = null;
     }
 
     // cria um arraylist de InternetAddress com os endereços de email
