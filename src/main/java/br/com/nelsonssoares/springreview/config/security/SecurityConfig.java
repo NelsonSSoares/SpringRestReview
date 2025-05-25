571caace8591501de297d785c9dd93652619a50a
Nelson <punkpunkada@gmail.com>
2025-05-25 10:21:11 -0300

    Authentication Service ok

--- src/main/java/br/com/nelsonssoares/springreview/config/security/SecurityConfig.java
+++ src/main/java/br/com/nelsonssoares/springreview/config/security/SecurityConfig.java
@@ -1,13 +1,21 @@
 package br.com.nelsonssoares.springreview.config.security;
 
+import br.com.nelsonssoares.springreview.security.jwt.JwtTokenFilter;
 import br.com.nelsonssoares.springreview.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,5 +35,35 @@ public class SecurityConfig {
         DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
         passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Enconder);
         return passwordEncoder;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtTokenFilter filter = new JwtTokenFilter(jwtTokenProvider);
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(
+                        session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers(
+                                        "/auth/sigin",
+                                                "/auth/refresh/**",
+                                                "/swagger-ui/**",
+                                                "/v3/api-docs/**",
+                                                "/auth/createUser"
+                                ).permitAll()
+                                .requestMatchers("/api/**").authenticated()
+                                .requestMatchers("/users").denyAll()
+                ).cors(cors ->{})
+                .build();
     }
 }
---
+++ src/main/java/br/com/nelsonssoares/springreview/services/AuthService.java
@@ -0,0 +1,38 @@
+package br.com.nelsonssoares.springreview.services;
+
+import br.com.nelsonssoares.springreview.domain.auth.dto.AccountCredentialsDTO;
+import br.com.nelsonssoares.springreview.domain.auth.dto.TokenDTO;
+import br.com.nelsonssoares.springreview.domain.repositories.UserRepository;
+import br.com.nelsonssoares.springreview.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository repository;
+
+
+    public ResponseEntity<TokenDTO> signin (AccountCredentialsDTO credentials){
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        credentials.getUsername(),
+                        credentials.getPassword()
+                )
+        );
+        var user = repository.findByUsername(credentials.getUsername());
+
+        if(user == null) throw new UsernameNotFoundException("Username not found");
+
+        var token = jwtTokenProvider.createAccessToken(user.getUsername(), user.getRoles());
+        return ResponseEntity.ok(token);
+    }
+}
