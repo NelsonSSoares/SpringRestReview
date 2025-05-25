3da76328efcaa9bda27fbac7d8ac323eb4c88674
Nelson <punkpunkada@gmail.com>
2025-05-25 08:17:36 -0300

    implementing security filter

---
+++ src/main/java/br/com/nelsonssoares/springreview/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,36 @@
+package br.com.nelsonssoares.springreview.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        var token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+
+        if(StringUtils.isNoneBlank(token) && jwtTokenProvider.validateToken(token)) {
+
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+
+            if(authentication != null){
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
